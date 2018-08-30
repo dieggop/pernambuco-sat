9de6159758c79c95bd3dc05c0aae012c902e97d3
Dieggo Carrilho <dieggopsc@gmail.com>
2018-08-30 20:34:13 -0300

    Primeiro commit

---
+++ src/main/java/com/br/carrilho/silva/pernambucosat/PernambucoSatApplication.java
@@ -0,0 +1,12 @@
+package com.br.carrilho.silva.pernambucosat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PernambucoSatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PernambucoSatApplication.class, args);
+	}
+}
