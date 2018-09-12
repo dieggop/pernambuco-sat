313a531f2e84492f1614aa022a3f063ab9895db0
Dieggo Carrilho <dieggopsc@gmail.com>
2018-09-11 22:42:43 -0300

    Ajustes em layout, menus e páginas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,11 @@
-insert into usuario (ativo,name,password,username,email,numero_telefone) values (1,'Dieggo Carrilho','$2a$10$uUjoIG3ZCXuL3McCOSjpw.aO51qeKzRtwsIfN.2qOstSFuZhhzRJC','dieggo','dieggo@email.com','997282363');
+insert into usuario (ativo,name,password,username,email,numeroTelefone) values (1,'Dieggo Carrilho','$2a$10$uUjoIG3ZCXuL3McCOSjpw.aO51qeKzRtwsIfN.2qOstSFuZhhzRJC','dieggo','dieggo@email.com','997282363')
 
-INSERT INTO REGRAS (regra) values ('admin_editar');
-INSERT INTO REGRAS (regra) values ('admin_adicionar');
-INSERT INTO REGRAS (regra) values ('admin_excluir');
-INSERT INTO REGRAS (regra) values ('admin_listar');
-INSERT INTO REGRAS (regra) values ('admin_mostrar');
-INSERT INTO REGRAS (regra) values ('USER');
-INSERT INTO REGRAS (regra) values ('ADMIN');
+INSERT INTO REGRAS (regra) values ('admin_editar')
+INSERT INTO REGRAS (regra) values ('admin_adicionar')
+INSERT INTO REGRAS (regra) values ('admin_excluir')
+INSERT INTO REGRAS (regra) values ('admin_listar')
+INSERT INTO REGRAS (regra) values ('admin_mostrar')
+INSERT INTO REGRAS (regra) values ('USER')
+INSERT INTO REGRAS (regra) values ('ADMIN')
 
-INSERT INTO USUARIO_REGRAS (usuarios_id, regras_id) values (1,6);
+INSERT INTO USUARIO_REGRAS (usuarios_id, regras_id) values (1,6)
